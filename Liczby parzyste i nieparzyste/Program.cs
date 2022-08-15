5412bea9ad567ba9e4177e35686015b6110df052
LAPTOP-M5O028QN\aleks <najwyzej@notowany.pl>
2022-08-15 14:22:59 +0200

    add project files again

---
+++ Liczby parzyste i nieparzyste/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liczby_parzyste_i_nieparzyste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz liczbę:");
+            var number = ParseNumber();
+            if (number % 2 == 0)
+                Console.WriteLine("Liczba jest parzysta.");
+            else
+                Console.WriteLine("Liczba jest nieparzysta.");
+
+            Console.ReadLine();
+        }
+        private static int ParseNumber()
+        {
+            var number = 0;
+            var correct = false;
+            while(!correct)
+            {
+                correct = int.TryParse(Console.ReadLine(), out number);
+                if (!correct)
+                {
+                    Console.WriteLine("Nieobsługiwany znak. Wpisz poprawną wartość.");
+                }
+            }
+            return number;
+        }
+    }
+}
